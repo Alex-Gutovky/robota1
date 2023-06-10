6d5864e8ae0e4d8205f38917f34c2df3996f8ac3
Alex-Gutovky <136168149+Alex-Gutovky@users.noreply.github.com>
2023-06-10 14:28:24 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,132 @@
+
+//что-бы заработали первые 5 заданий надо удалить вторую часть
+// и наоборот
+
+
+
+
+
+//$('div').click(function() { 
+  //$(this).hide(); 
+  //}); 
+  
+ // $('h1').click(function() { 
+   // $(this).show(); 
+   //}); 
+
+
+   // $('.div').click(function() { 
+ // $('.div').hide(2000); 
+  //}); 
+
+  // $('p').click(function() { 
+  //  $('.div').show(1300); 
+    //}); 
+
+   // $('.div1').click(function() { 
+    //  $(this).slideUp(5000); 
+     // }); 
+    
+      // $('.p2').click(function() { 
+     //   $('.div1').slideDown(1300); 
+      //  }); 
+
+     // $('.div2').click(function() { 
+     //   $(this).fadeIn(1000, 0,5);
+     // }); 
+    
+      // $('.p3').click(function() { 
+      // $('.div2').fadeOut(2000,0.5); 
+      //  }); 
+
+
+
+       //$('.item1').click(function() { 
+       //   $('.item-block').slideToggle(2000); 
+         // $('.item-block2').slideUp(2000); 
+         // $('.item-block3').slideUp(2000); 
+          //$('.item-block4').slideUp(2000); 
+          //}); 
+         // $('.item2').click(function() { 
+           // $('.item-block2').slideToggle(2000); 
+            //$('.item-block').slideUp(2000); 
+           
+         //   $('.item-block3').slideUp(2000); 
+           // $('.item-block4').slideUp(2000); 
+
+            //}); 
+            //$('.item3').click(function() { 
+            //  $('.item-block3').slideToggle(2000); 
+           //   $('.item-block').slideUp(2000); 
+             /// $('.item-block2').slideUp(2000); 
+              
+             // $('.item-block4').slideUp(2000); 
+             // }); 
+              //$('.item4').click(function() { 
+                ////$('.item-block4').slideToggle(2000); 
+               // $('.item-block').slideUp(2000); 
+                //$('.item-block2').slideUp(2000); 
+                //$('.item-block3').slideUp(2000); 
+              
+               /// }); 
+        
+
+
+
+              // for(let i =1;i <=9;i++){
+                //$(`#item${i}`).css({'background':`url(img/image_part_00${i}.png)`,
+                
+                //'background-size':'100% 100%'})
+              // }
+              // for(let i =10;i <=24;i++){
+               // $(`#item${i}`).css({'background':`url(img/image_part_0${i}.png)`,
+                
+                //'background-size':'100% 100%'})
+               //}
+
+
+  //for(let i =1;i <=18;i++)
+  //{
+   // $(`#item${i}`) .draggable({
+   //   scope:`cell${i}`,
+  //    containment:pole,
+   //   snap: ".drop-item",
+    //  snapMode:"inner",
+    //  snapTolerance:30
+    //})  
+    //$(`#drop${i}`).droppable({
+    //  scope:`cell${i}`,
+    // drop: function(event,ui){
+      // let id = ui.draggable.attr('id')
+      // $(`#${id}`).draggable('disable')
+    //},
+    //tolerance: 'fit'
+    //}); 
+  //}
+  
+   //for(let i =10;i <=24;i++)
+   //{
+   //  $(`#item${i}`) .draggable({
+    //   scope:`cell${i}`,
+     //  containment:pole,
+      // snap: ".drop-item",
+      // snapMode:"inner",
+      // snapTolerance:30
+    // })  
+     //$(`#drop${i}`).droppable({
+      // scope:`cell${i}`,
+      //drop: function(event,ui){
+      //  let id = ui.draggable.attr('id')
+      //  $(`#${id}`).draggable('disable')
+    // },
+    // tolerance: 'fit'
+    // }); 
+  // }
+  // $('.drop-area').sortable()
+
+
+
+
+
+
+  
